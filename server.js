13bfcf2afbc313771bb1174368d881bbd8f6f456
Gaurav <gsaraf895@gmail.com>
2021-06-24 21:03:44 +0530

    port changed

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
 import app from "./app.js";
 
-app.listen(3000, console.log(`Sever running on PORT 3000 `.blue.bold));
+app.listen(
+  process.env.PORT || 3000,
+  console.log(`Sever running on PORT 3000 `.blue.bold)
+);
